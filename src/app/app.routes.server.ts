cfad515712277d8082bf4b6b1861f7a964beff5e
Natto <thisis@natalies.email>
2025-05-19 11:07:11 -0700

    Do not attempt to prerender dynamic routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'object/:id',
+    renderMode: RenderMode.Server, // SSR only, not prerendered
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];

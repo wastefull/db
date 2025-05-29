8f5914bcaf32e3e9485bc95b4813667582df303b
Natto <thisis@natalies.email>
2025-05-29 13:09:28 -0700

    Cleaning up links and favicon

--- src/app/home/pillgroup/pillgroup.component.ts
+++ src/app/home/pillgroup/pillgroup.component.ts
@@ -5,12 +5,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-pillgroup',
   imports: [CommonModule],
   templateUrl: './pillgroup.component.html',
-  styleUrl: './pillgroup.component.scss'
+  styleUrl: './pillgroup.component.scss',
 })
 export class PillgroupComponent {
-  links = [
-    { title: 'Link 1', link: 'https://angular.dev' },
-    { title: 'Link 2', link: 'https://angular.dev/tutorials' },
-    { title: 'Link 3', link: 'https://angular.dev/tools/cli' },
-  ]
+  links = [{ title: 'Wastefull', link: 'https://wastefull.org' }];
 }
